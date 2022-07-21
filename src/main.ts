3132c98e2864f7418fffa59a1e5da40271656b0e
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-21 17:50:15 +0900

    cors設定追加

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   app.use(cookieParser());
+
+  // corsの許可
+  app.enableCors({
+    origin: '*',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  });
 
   await app.listen(3000);
 }
