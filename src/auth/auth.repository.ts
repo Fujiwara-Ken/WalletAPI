32b517aa0aee872760289cfe221ab70f708b1477
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-21 17:47:06 +0900

    bcrypt周りを一旦削除

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,15 +1,17 @@
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
 
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { email, password } = createUserDto;
-    const salt = await bcrypt.genSalt();
-    const hashPassword = await bcrypt.hash(password, salt);
+    // const salt = await bcrypt.genSalt();
+    // const hashPassword = await bcrypt.hash(password, salt);
 
-    const user = this.create({ email, password: hashPassword });
+    // const user = this.create({ email, password: hashPassword });
+
+    const user = this.create({ email, password });
 
     await this.save(user);
     return user;
