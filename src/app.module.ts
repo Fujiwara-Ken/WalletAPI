4e21de845d6367205964f9ab9959258af01c13d2
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-21 20:56:33 +0900

    docker mysql接続

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'wallet-db',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'develop',
+      synchronize: true,
+      logging: false,
+      entities: [User],
+      migrations: ['src/migration/*.ts'],
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
