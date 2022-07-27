31469ef51f543b2220f92a1267730abab6a05fc3
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-27 23:58:56 +0900

    get user info c add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { GetTokenAmountDto } from './dto/get-token-amount.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('get-user-info/:user_id')
+  async getUserInfo(
+    @Param('user_id', ParseIntPipe) user_id: number
+  ): Promise<Object> {
+    const user = await this.userService.getUserInfo(user_id);
+    return user;
+  }
 
   // @UseGuards(JwtAuthGuard)
   @Post('get-token-amount')
