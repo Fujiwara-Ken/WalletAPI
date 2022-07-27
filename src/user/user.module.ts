8e4ebebed112c3150c763fca4d0a657fcb2eb000
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-27 15:51:31 +0900

    env module add

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
