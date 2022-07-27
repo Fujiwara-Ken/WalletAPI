da66f6a552066c0959593be318d2aeb705b16780
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-27 14:34:29 +0900

    encrypt script add

--- src/user/solana/getTokenAmount.ts
+++ src/user/solana/getTokenAmount.ts
@@ -4,9 +4,11 @@ import { Account } from '@solana-suite/core';
 export const getTokenAmount = async (walletAddress: string) => {
   const mint = process.env.TOKEN_KEY as string;
 
-  const hist1 = await Account.getTokenBalance(
+  const amount = await Account.getTokenBalance(
     mint.toPublicKey(),
     walletAddress.toPublicKey()
   );
-  console.log('# token history by publish: ', hist1.unwrap());
+  console.log('# token history by publish: ', amount.unwrap());
+
+  return amount;
 };
