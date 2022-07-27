067683e4035a3122f9d5395edb5aeb3b516f302c
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-27 15:56:45 +0900

    get token amount dto param fix

--- src/user/solana/getTokenAmount.ts
+++ src/user/solana/getTokenAmount.ts
@@ -3,6 +3,7 @@ import { Account } from '@solana-suite/core';
 // 指定したSPLトークンの合計を取得
 export const getTokenAmount = async (walletAddress: string) => {
   const mint = process.env.TOKEN_KEY as string;
+  console.log('wallet', walletAddress);
 
   const amount = await Account.getTokenBalance(
     mint.toPublicKey(),
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { getTokenAmount } from './solana/getTokenAmount';
 import { UserRepository } from './user.repository';
+import { GetTokenAmountDto } from './dto/get-token-amount.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  public async getTokenAmount(walletAddress) {
-    const tokenAmount = getTokenAmount(walletAddress);
+  public async getTokenAmount(getTokenAmountDto: GetTokenAmountDto) {
+    const tokenAmount = getTokenAmount(getTokenAmountDto.walletAddress);
     console.log('service', tokenAmount);
     return tokenAmount;
   }
