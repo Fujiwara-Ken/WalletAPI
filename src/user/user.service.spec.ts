d33e6b22f3c31c93cbf604f4bff9b7abf0f18af7
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-08-02 18:33:19 +0900

    user service spec add

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
+import { NotFoundException } from '@nestjs/common';
+
+const mockUser1 = {
+  id: 1,
+  email: 'aaa@email.com',
+  password: 'password',
+  wallet_address: 'walletAddresswalletAddresswalletAddress',
+};
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+  });
+
+  describe('getUserInfo', () => {
+    it('正常系', async () => {
+      const expected = {
+        userId: 1,
+        email: 'aaa@email.com',
+        password: 'password',
+        wallet_address: 'walletAddresswalletAddresswalletAddress',
+      };
+
+      userRepository.findOne.mockResolvedValue(expected);
+      const result = await userService.getUserInfo(1);
+      expect(result).toEqual(expected);
+    });
+
+    it('異常系: ユーザーが存在しない', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+      await expect(userService.findById(100)).rejects.toThrow(
+        NotFoundException
+      );
+    });
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 });
