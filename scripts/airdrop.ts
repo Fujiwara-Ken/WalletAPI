607db5b36818815b98617ca3e000bff9f8474f20
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-08-24 10:43:10 +0900

    indent fix

--- scripts/airdrop.ts
+++ scripts/airdrop.ts
@@ -7,19 +7,13 @@ import 'dotenv/config';
 
 // 開発用の SOL を1SOL取得
 const airdrop = async () => {
-  const owner = new KeypairStr(
-    process.env.SYSTEM_WALLET_ADDRESS || '',
-    process.env.SYSTEM_WALLET_SECRET || ''
-  );
+  const owner = new KeypairStr(process.env.SYSTEM_WALLET_ADDRESS || '', process.env.SYSTEM_WALLET_SECRET || '');
 
   console.log('#owner', owner);
 
   await Account.requestAirdrop(owner.toPublicKey());
 
-  console.log(
-    '# owner balance: ',
-    await Account.getBalance(owner.toPublicKey())
-  );
+  console.log('# owner balance: ', await Account.getBalance(owner.toPublicKey()));
 };
 
 airdrop();
