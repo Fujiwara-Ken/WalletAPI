031c8dc28aa418c9b31adce18bbb0def39b776b9
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-08-25 13:49:39 +0900

    indent fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { TokenAmount } from '@solana/web3.js';
 
 import { User } from 'src/entities/user.entity';
@@ -24,21 +17,15 @@ export class UserController {
   }
 
   @Get('user-info/:userId')
-  async getUserInfo(
-    @Param('useId', ParseIntPipe) userId: number
-  ): Promise<User> {
+  async getUserInfo(@Param('useId', ParseIntPipe) userId: number): Promise<User> {
     const res = await this.userService.getUserInfo(userId);
     return res;
   }
 
   // @UseGuards(JwtAuthGuard)
   @Post('get-token-amount')
-  async getTokenAmount(
-    @Body() getTokenAmountDto: GetTokenAmountDto
-  ): Promise<TokenAmount> {
-    const userTokenAmount = await this.userService.getTokenAmount(
-      getTokenAmountDto
-    );
+  async getTokenAmount(@Body() getTokenAmountDto: GetTokenAmountDto): Promise<TokenAmount> {
+    const userTokenAmount = await this.userService.getTokenAmount(getTokenAmountDto);
     return userTokenAmount;
   }
 }
