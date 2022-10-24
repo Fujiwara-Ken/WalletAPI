8dc1525c576453bc151a1e775e27b7823d15119f
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-10-24 16:48:53 +0900

    sign up add

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,13 +2,16 @@ import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AppDataSource } from '../data-source';
+import { createWallet } from './solana/createWallet';
 
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const user = new User();
+    const walletAddress = createWallet();
 
     user.email = createUserDto.email;
     user.password = createUserDto.password;
+    user.walletAddress = (await walletAddress).pubkey;
 
     AppDataSource.manager.save(user);
     return user;
