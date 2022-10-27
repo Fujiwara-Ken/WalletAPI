f3051cbce21d887419c64da7f92345eddffc9667
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-10-27 23:03:02 +0900

    seeder add

--- src/seeders/users/data.ts
+++ src/seeders/users/data.ts
@@ -1,10 +1,19 @@
 import { User } from '../../entities/user.entity';
 
-export const users: User[] = [{}];
+export const users: User[] = [{
+  userId: 1,
+  email: "sample@email.com",
+  password: "password",
+  walletAddress: "walletAddress",
+  refreshToken: "fdb8fdbecf1d03ce5e6125c067733c0d51de209c",
+  refreshTokenExp: "a",
+  createdAt: new Date("2022-03-08 12:37:51.250062"),
+  updatedAt: new Date("2022-03-08 12:37:51.250062")
+];
 
 /**
  * タスクの型定義 src/tasks/interfaces/task.interface.ts に置いてある
  */
-// export interface ITask {
-//   name: string;
-// }
+export interface ITask {
+  name: string;
+}
